4d61409818aa4019605d874a13b6cb8ce2bbca2f
dmmiller612 <dmmiller612@gmail.com>
2019-02-14 10:41:47 -0500

    updated docker and setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='taskdl',
-      version='0.1.4',
+      version='0.1.5',
       description='Deep learning on Lifeomic',
       keywords = ['tensorflow', 'machine learning', 'lifeomic', 'deep learning', 'pytorch'],
       url='https://github.com/lifeomic/TaskDeepLearning',
-      download_url='https://github.com/lifeomic/TaskDeepLearning/archive/0.1.4.tar.gz',
+      download_url='https://github.com/lifeomic/TaskDeepLearning/archive/0.1.5.tar.gz',
       author='Derek Miller',
       author_email='dmmiller612@gmail.com',
       install_requires=['requests'],
